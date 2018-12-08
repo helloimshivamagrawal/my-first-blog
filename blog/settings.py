cf9bac81e30259eb5f00a8dfe34da5554d4617b9
HelloIMShivamAgrawal <problem.withamazon@gmail.com>
2018-12-09 03:39:43 +0530

    set debug mode as False

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n7unhpk@fl8qa8n6y=!9q**3c6%(4^*2(*_z^nos92!#%d*kut'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
